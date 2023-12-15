785d24215f8e39a3fe1331a1d9403b0d6952e8b9
Riccardo Passoni <riccardopassoni@protonmail.com>
2023-12-15 11:56:46 +0100

    separated timestamps and lyrics

--- LyriFont.py
+++ LyriFont.py
@@ -1,3 +1,9 @@
 import syncedlyrics
-lrc = syncedlyrics.search("[The Beatles] [Hard Day's Night]")
-print(lrc)
+
+lrc = syncedlyrics.search("[The Beatles] [Hard Day's Night]").splitlines()
+
+timestamps = [x[0:10] for x in lrc]
+lyrics = [x[11:len(x)] for x in lrc]
+
+print(timestamps)
+print(lyrics)
