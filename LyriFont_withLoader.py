68689c5b53c90669b4cb43014ddfd9c148e4b0bb
Riccardo Passoni <riccardopassoni@protonmail.com>
2023-12-17 16:06:40 +0100

    spotify fetch genre

--- LyriFont_withLoader.py
+++ LyriFont_withLoader.py
@@ -2,6 +2,8 @@ import argparse
 import syncedlyrics
 import os
 import pandas as pd
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 from pythonosc import udp_client
 from pythonosc import osc_server
 from pythonosc import dispatcher
@@ -9,17 +11,41 @@ import sys
 
 currentpath = sys.path[0]
 
-
 ##PATH LOCALE!!!
 excel_path = currentpath + "\\ML_Spreadsheet.xlsx" ##PATH LOCALE !!!!!!
 ##PATH LOCALE!!
-
-
 
 ## Read the file
 df = pd.read_excel(excel_path, index_col=None, header=None)
 
 client = udp_client.SimpleUDPClient("127.0.0.1", 1234)
+
+def getSpotifyFont(artist):
+
+  client_id = "e118ebb8f8984e75b253ba9bb60e8da3"
+  client_secret = "0cca8ed331d24a17a9f22decbe2fa5d3"
+  client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+  sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager) #spotify object to access API
+  name = artist
+  result = sp.search(name)
+  track = result['tracks']['items'][0]
+
+  artist = sp.artist(track["artists"][0]["external_urls"]["spotify"])
+  return(artist["genres"][0].capitalize())
+
+def excel(genre):
+  mask = df[1].str.contains(genre, na=False)
+
+  # If the target string is found, get the first element of its row
+  if mask.any():
+      # Find the index of the first occurrence
+      index_of_first_occurrence = df.index[mask].tolist()[0]
+        
+      # Get the first element of the row in column A (assuming column A contains the desired data)
+      return (df.iloc[index_of_first_occurrence, 0])
+    
+
+
 
 def loadLyrics(unused_addr, args):
   
@@ -27,12 +53,10 @@ def loadLyrics(unused_addr, args):
   artistname = fname.split(" - ")[0]
   songname = os.path.splitext("".join(fname.split(" - ")[1:]))[0]
   
-  songFont_df = (df[df[1]== fname])
-  
-  songFont = songFont_df.iloc[0, 0]
-  
-  ##print(songFont.iloc[0, 0])
-  
+  genre = getSpotifyFont(artistname)
+  print(genre)
+  songFont = excel(genre)
+
   print("Loading Lyrics and Timestamps...")
   lrc = syncedlyrics.search("["+artistname+"] ["+songname+"]").splitlines()
   timestamps = [x[1:9] for x in lrc]
